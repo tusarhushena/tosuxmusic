75e30e811200fa4e09e4e013c551d4ade9671fea
tosu <154593488+tusarhushena@users.noreply.github.com>
2024-09-03 22:54:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ API_ID = 24620300
 API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "6915408680:AAFWJUWsFH6kqAgjv26Xlfzq1LP0QYbeDpY"
+BOT_TOKEN = "6457568929:AAEjV58bTPE-ioYY6TFX4VvuPOlXOHY7ytc"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "mongodb+srv://fidixi3663:w7rvlxmDd5lsX9ix@cluster0.0k1an50.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URI = "mongodb+srv://Zenitsuaf:Zenitsuaf@cluster0.i58aapw.mongodb.net/?retryWrites=true&w=majority"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 9999))
 
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 9999))
 LOGGER_ID = -1002023182491
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = 6848223695
+OWNER_ID = 5322882174
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/about-tosu/tosu",
+    "https://github.com/tusarhushena/tosu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_tosuu")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/nothing_bots_support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Dub_Anime_In_Hindi_India")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/RDJ_Anime_Group")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -62,8 +62,8 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
-STRING1 = "BQF3rQwAu4oMxPa0v9gBNFS6INfVYVf_JE1IvU4h7bpjk5nrN778FfuLAnzQxQtgs9H18AKrGWmG4a5MS-7c353F4qG3_xvVg1-HpqrG48bVZPbRR7Frvo0XfXAY9rzQtRbnP4QhVsk_iXokcll-XO7-uyzxVa-kAJh10Evxt2k2MDXQlZyaalGlKwIPxOgQSRp11i0U0cd8avLLlOBi8HkHCSJyhzHEmke-O6VJPQkrXOTmvwG8WW1JX2amkAntXape5ioDnGwp5fQFcaCsJHwm52Y9SYTFoDtjGRph4IIdKYhrze3hc5eDmsqs3zTcyjyQ1x0SX2LfEEMWBRQQMDml0ix4HgAAAAE1mGFGAA"
-STRING2 = "BQF3rQwAZrQtzPqDkma6TUD5QxUjzGq5hAaeDH2nY_eN-D38eBIFFJH7Ha76eSQtLNvxMUrRrFOKKar6RjEfr_xoaBBmO_j2VbzMhw4sI1Z03lIGYU-OjekptdPgantGeMW9Qn7DmoHhsKJpUNe_KwlGGD3SM1qD1v0ZGr-1fSZtg9IxAX0JmcncNZWinG57mFizkPGuPYr2jl9MleHH6XL6yXAdNDiYplaOHD8trZKQCgBkVgr_ABajcim-mXpd0g82XFtb5Cv8kF7kF-e0rgbG4XealK2cMC83JD4G3-8eWfYbWsRg01_fkHFq5UAA_UJN6BX0YBXS8GUpE9nB0lel_z2RtgAAAAGhjpphAA"
+STRING1 = ""
+STRING2 = ""
 STRING3 = ""
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
