792513cba8ac6a441d7734fbb77595fd44302c34
tosu <154593488+tusarhushena@users.noreply.github.com>
2024-09-04 15:20:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,21 +78,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/adb4c3fe1124461b023c4.jpg"
+    "START_IMG_URL", "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/531090c47c12e983bf731.jpg"
+    "PING_IMG_URL", "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/6dd4127abb744a1c82ce9.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph//file/4f8a07408aacfff00f3de.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph//file/4f8a07408aacfff00f3de.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
+STATS_IMG_URL = "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
+STREAM_IMG_URL = "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/3d8ca7058b4a72925bf8c.jpg"
 
 
 def time_to_seconds(time):
